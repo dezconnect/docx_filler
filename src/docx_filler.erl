0dd94c036b7c0793476c188d61aa9f6c21da41db
Alex V Gonchar <dezconnect@gmail.com>
2021-10-26 00:19:02 +0300

    Added naive unzip/zip realization (not working yet)

--- src/docx_filler.erl
+++ src/docx_filler.erl
@@ -1,15 +1,97 @@
 
 -module(docx_filler).
--export([template/3, replaceValues/2, verify/2]).
+-export([template/3, zip_template/3]).
+
+%% 
+%% Итого чтобы успешно сделать замену необходимо:
+%% 1. Раскукожить файлик в произвольный каталог (Сгенерировать временное называние из словаря или передавать uuid ?)
+%% 2. Подменить через :template document.xml 
+%% 3. Собрать файл обратно в файл из каталога
+%%
+
 
 %%
 % Function to handling undefined behaviour
-%
+% TODO: Add normal error handling 
 generate_exception(Reason) ->
   {error, Reason}.
 
+
 %%
-% Exported function for external applications
+% Exported function for external applications for Microsoft Word .docx files
+%
+zip_template(TemplateFile, DestinationFile, Params) ->
+  case unzipFile(TemplateFile) of 
+    {ok, DirName, FileList} -> 
+      DocumentFile = DirName ++ "document.xml",
+      template(DocumentFile, DocumentFile, Params),
+			zipDirectory(DestinationFile, FileList),
+			deleteDirectory(DirName); 
+    {error, Reason} ->
+      generate_exception(Reason)
+  end.
+
+
+%% 
+% Function for unzip template file to named directory
+%
+unzipFile(TemplateFile) ->
+  case zip:unzip(TemplateFile, [{cwd, "./tmp/"}]) of
+		{ok, FileList} ->
+			{ok, "./tmp/word/", FileList};
+		{error, Reason} ->
+			generate_exception(Reason)
+	end.
+
+
+%% 
+% Zipping directory to file-container
+%
+zipDirectory(DestinationFile, FileList) ->
+  case zip:create(DestinationFile, FileList) of 
+		{ok, _} ->
+			ok;
+		{error, Reason} ->
+			generate_exception(Reason)
+	end.
+
+%%
+% Check filetype 
+%
+isFileOrLink(File) -> filelib:is_regular(File) or (ok == element(1, file:file_readlink(File))).
+
+
+%%
+
+deleteDirectory(DirectoryName) ->
+  case {filelib:is_dir(DirectoryName), isFileOrLink(DirectoryName)} of
+    {_, true} -> throw("The specified path is not a directory");
+    {true, _} -> recursiveDeleteDirectory([DirectoryName]);
+    {false, _} -> ok
+  end.
+
+
+ensureDirSlash(Dir, Child) ->
+  case lists:suffix("/", Dir) of
+    true -> Dir ++ Child;
+    false -> Dir ++ "/" ++ Child
+  end.
+
+recursiveDeleteDirectory([]) -> ok;
+recursiveDeleteDirectory([Current | Rest]) ->
+	{ok, ChildrenNames} = file:list_dir(Current),
+  Children = lists:map(fun(File) -> ensureDirSlash(Current, File) end, ChildrenNames),
+  {FilesOnly, DirsOnly} = lists:partition(fun isFileOrLink/1, Children),
+  lists:foreach(fun(File) -> ok = file:delete(File) end, FilesOnly),
+
+  if
+    length(DirsOnly) == 0 -> ok = file:del_dir(Current), recursiveDeleteDirectory(Rest);
+    true -> recursiveDeleteDirectory(DirsOnly ++ [Current] ++ Rest)
+  end.
+
+
+%%
+% Exported function for external applications for plain-text files 
 %
 template(TemplateFile, DestinationFile, Params) ->
   case readKeywords(TemplateFile) of
@@ -88,4 +170,5 @@ replaceValues(Content, Params) ->
 replaceValue(Content, Key, Value) -> 
   ReTemplate = "\{\{" ++ atom_to_list(Key) ++ "\}\}",
   re:replace(Content, ReTemplate, Value, [{return, list}]).
+
 
